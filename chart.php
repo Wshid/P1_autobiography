df3754f8fa44a6f6b87f5acf144ec6de2e724a84
wshid <kso4013@gmail.com>
2017-01-11 04:55:51 +0000

    Chart init version complete

--- chart.php
+++ chart.php
@@ -32,8 +32,9 @@
 
     <!-- Custom styles for this template -->
     <link href="css/block.css" rel="stylesheet">
+    <link href="css/header_navbar.css" rel="stylesheet">
     <link href="css/columns_l4.css" rel="stylesheet">
-    <script src="js/vender/jquery-3.1.1.min.js"></script>
+    <link href="css/chart.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -63,8 +64,8 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse"><!-- id="navbar" -->
               <ul class="nav navbar-nav">
-                <li><a href="block.php">Block</a></li>
-                <li class="active"><a href="chart.php">Chart</a></li>
+                <li class="active"><a href="block.php">Block</a></li>
+                <li><a href="chart.php">Chart</a></li>
                 <li><a href="word.php">Word</a></li>
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
@@ -86,15 +87,15 @@
                         //echo "<a href='login.php'>로그인 해주세요</a>";
                   ?>
                 </span>
-                <span><a class="btn btn-lg btn-theme login_out" href="login.php" role="button">Login</a></span>
-                <span class="signinName login_out">  
+                <span><a class="btn btn-lg btn-primary login_out" href="login.php" role="button">Login</a></span>
+                <span class="login_out">  
                   <?php
                     }
                     else{
                       echo "Hi!, ".$_SESSION['name']." :)";
                   ?>
                 </span>  
-                <span><a class="btn btn-lg btn-primary login_out" href="logout.php" role="button">Logout</a></span>
+                <span><a id="logout_but" class="btn btn-lg btn-primary login_out" href="logout.php" role="button">Logout</a></span>
                   <?php
                     }
                 ?>
@@ -114,47 +115,25 @@
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
       </div>
     </div>
+    
+    <span id="chart_title">Please Select #Tag</span>
     <div class="container"> <!-- Container가 존재 해야 내부 객체가 정렬이 됨 -->
+
+    <!--<hr style='margin-top: 0px'>-->
+    <form class="filter-form" id="form_filter">
+      <!--
+      <span class="form-item">
+        <input type="radio" name="filter" id="filter-all" value="all" checked>
+        <label for="filter-all">All</label>
+      </span>-->
+    </form>
       <div class="blockContainer">
         <!-- jquery로 값이 추가 될 부분 -->
         <!-- 우선 +에 해당하는 블럭을 추가시켜 놓아야함 -->
         <!-- 이 내용들도 JQuery 해야 할 듯한데..
             JQuery객체로 가져오게 되면 완전 태그형으로 변환 되니까 -->
         </div>
-      <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-          Launch demo modal
-      </button>
     </div>
-    <!-- Modal -->
-      <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-            </div>
-            <div class="modal-body">
-              <h3>Text in a modal</h3>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-              <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="top" title="Tooltip on top">Tooltip on top</button>
-              <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
-            </div>
-            <div class="modal-footer">
-              <a href="#" data-toggle="tooltip" title="Some tooltip text!">Hover over me</a>
-      
-                <!-- Generated markup by the plugin -->
-                <div class="tooltip top" role="tooltip">
-                  <div class="tooltip-arrow"></div>
-                  <div class="tooltip-inner">
-                    Some tooltip text!
-                  </div>
-                </div>
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
-        </div>
-      </div>
       <hr>
 
       <footer>
@@ -166,6 +145,7 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/vender/jquery-3.1.1.min.js"></script>
     <script src="js/vender/bootstrap.min.js"></script>
     <script src="js/vender/jquery.ba-throttle-debounce.min.js"></script>
     <script src="js/vender/jquery-ui.min.js"></script>
@@ -174,118 +154,6 @@
     <script src="js/block.js"></script>
     <script src="js/mk_modal.js"></script>
     <script src="js/input_check.js"></script>
-    <script>
-      $(function(){
-        //alert("Inner JQuery");
-        var $blockContainer=$('.blockContainer'),
-            line_end=1,
-            line_start=2,
-            name='<?php echo($_SESSION["name"]); ?>'; // js 내부에서 php 구문 실행이 가능하다.
-        
-        get_block_json(name);
-        
-        
-        // JQUERY로 해서 객체로 받아내면 더 완벽했을텐데.. 아쉽다
-        function get_block_json(name){ 
-          var plusTitle="ADD YOUR BLOCK! :)",
-            plusSub="Some Information Required",
-            id_add_string="add";
-          var plusHTML='<div class="columns_l4">'+
-                        '<div class="row">'+
-                          '<div class="col-md-4" data-toggle="modal" data-target="#modal_'+id_add_string+'">'+ /* 버튼 없이도 됨! 이득! :) */
-                            '<div class="row_inner" id="plus_block">'+ /* add_block으로 명명하면 어디선가 충돌이 나는 듯 하다 */
-                              '<h3>'+plusTitle+'</h3>'+
-                              '<h5>'+plusSub+'</h5>'+
-                            '</div>'+
-                          '</div>'; // div가 두개 모자름, 추후 추가함
-          var modal4_string=["Title", "SubTitle", "Body", "Tag"]; //여기서 에러난게 아냐
-          //console.log(modal4_string);
-          plusHTML+=mk_modal_form_string(id_add_string, plusTitle, plusSub, modal4_string, "save_close", -1); // is_modify=-1
-          //var modifyHTML=mk_modal4_string(id_add_block, plusTitle, plusSub, )
-            
-            $.getJSON('json/blocks/block_'+name+'.json', function(data){
-                var length=data.length, 
-                  elements=plusHTML; // 배열이 아닌 string을 사용한다.
-               
-                //console.log('json/blocks/block_'+name+'.json');
-                $.each(data, function(i, item){
-                    var itemHTML="";
-                    //var modifyHTML="";
-                    var button_type="modify_delete"
-                    var idx_string=""
-                    var col3_base=i%3; // 3열 체크시 사용한다.
-                    var tag_raw=item.tag;
-                    
-                    console.log(tag_raw);
-                    var tag="";
-                    for(var tag_idx in tag_raw){
-                      tag+=tag_raw[tag_idx]+" ";
-                    }
-                    
-                    tag=tag.trim();
-                    
-                    var modalHTML=mk_modal_string(i, item.title, item.subtitle, item.body, tag, button_type, item.user_idx);
-                    var block_idx=item.user_idx; //form에 임의 저장할 값
-                    
-                    
-                    itemHTML+=check_line(col3_base, line_start, 1); // 1 : check_line_start
-                    
-                    idx_string="modal_view_"+i;
-                    /*
-                    if(button_type=="save_close"){
-                      idx_string="modal_modify_"+i;
-                      idx_string="modal_view_"+i;
-                    }else{
-                      alert("ERROR ON BUTTON TYPE");
-                    }*/
-                    // grid 생성하는 부분
-                    itemHTML+='<div class="col-md-4" data-toggle="modal" data-target="#'+idx_string+'">'+ /* 버튼 없이도 됨! 이득! :) */
-                                '<div class="row_inner">'+
-                                  '<h3>'+item.title+'</h3>'+
-                                  '<h5>'+item.subtitle+'</h5>'+
-                                '</div>'+
-                            '</div>'+modalHTML;        
-                   
-                    itemHTML+=check_line(col3_base, line_end, 2);
-                    
-                    //console.log(col3_base);
-                    // *** 추후 제거 할 부분, 테스트 및 누수를 확인함 ***//
-                    
-                    itemHTML+=input_blank(i, length-1, col3_base, line_start, line_end);
-                    
-                    // 여기에 modify관련 modal을 만들어주어야 함
-                    //modifyHTML+=mk_modal_form_string(i, plusTitle, plusSub, modal4_string, "save_close", item.user_idx);
-                      // 안쓰는데?
-                    //itemHTML+="</div></div>";
-                    elements+=itemHTML; // JQuery 객체화를 피하기 : 단순 문자열로 처리한다.
-                   //console.log(elements); 
-                   /*for(i=0; i<$(itemHTML).length;i++){
-                       elements.push($(itemHTML).get(i)); // 여기 부분에서 원활하게 값을 못가져옴, JQuery 객체화 하면, 임의대로 태그를 완성시킴 
-                   }*/ // 문제 해결, JQUERY 화를 하지 않음
-                   
-                   
-                });
-                //console.log(elements);
-                $blockContainer.append(elements);
-                $blockContainer.append(mk_modal_success("title")); // SUCCESS 모달 생성, mk_modal에 지정
-                box_activing();
-                
-            });
-            
-        }
-        
-        function box_activing(){  //block.js와 중복됨 //
-            $('.columns_l4 .col-md-4').each(function(){
-                //console.log("BOX_ACTIVE");
-                $(this).on('mouseover', function(){
-                    $(this).addClass('box_active');
-                })
-                .on('mouseleave', function(){
-                    $(this).removeClass('box_active');
-                });
-            });
-        }
-      });
-    </script>
+    <script src="js/chart.js"></script>
   </body>
 </html>
